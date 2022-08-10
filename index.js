ca8400a44e44d847efd74c3fa499b72f35e75457
codingvaleria <codingvaleria@gmail.com>
2022-08-10 16:32:34 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,54 @@
-// Code your solutions in this file
+// for loop
+// for (let age = 30; age < 40; age++) {
+
+//     console.log(`I'm ${age} years old. Happy birthday to me!`);
+// }
+
+//gifts
+
+// const gifts = ["teddy bear", "drone", "doll"];
+
+// function wrapGifts(gifts) {
+
+//     for (let i = 0; i < gifts.length; i++) {
+
+//         console.log(`Wrapped ${gifts[i]} and added a bow!`);
+
+//     }
+
+//     return gifts;
+// }
+
+// wrapGifts(gifts);
+
+
+//writeCards
+
+function writeCards(stringNames, eventName) {
+    const messages = [];
+
+    for (let counter = 0; counter < stringNames.length; counter++) {
+        let name = stringNames[counter];
+        let message = `Thank you, ${name}, for the wonderful ${eventName} gift!`;
+        messages.push(message);
+    }
+
+    return messages;
+
+}
+
+console.log(writeCards(["Linda", "Alice", "Val"], "surprise"));
+
+
+//CountDown
+
+
+function countDown(positiveInteger) {
+    let x = 10;
+    while (x >= 0) {
+
+        console.log(x--);
+    }
+
+}
+countDown(10);
